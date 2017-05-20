656a1410372710b5c7ee561ce1938dd9c45ed54a
kuroyakov <k.kuroya@gmail.com>
2017-05-20 20:27:41 +0900

    Add kotlin example

---
+++ kotlin/hello.kt
@@ -0,0 +1,7 @@
+// 実行方法は次の通り
+// How to execute this example (for commandline user)
+// $ kotlinc-jvm <source.kt> -include-runtime -d <outfile.jar>
+// $ kotlin <outfile.jar>
+fun main(args:Array<String>){
+    println("Hello, Kotlin")
+}
