ff48f91e0eda1a005f03936b83d71c587f88de40
kuroyakov <k.kuroya@gmail.com>
2017-05-21 20:44:09 +0900

    Add alternatives for kotlin example

--- kotlin/hello.kt
+++ kotlin/hello.kt
@@ -1,6 +1,10 @@
 // How to execute this example (for commandline user)
+// Option 1:
 // $ kotlinc-jvm <source.kt> -include-runtime -d <outfile.jar>
 // $ kotlin <outfile.jar>
+// Option 2:
+// $ kotlinc <source.kt>       <-- outfile will be "SourceKt.class"
+// S kotlin <outfile>          <-- without [.class]
 fun main(args:Array<String>){
     println("Hello, Kotlin")
 }
