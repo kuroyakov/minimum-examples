03eec7510c82387c34f7d4bf0646727b0931b7b5
kuroyakov <k.kuroya@gmail.com>
2017-05-24 21:37:55 +0900

    Add timer to evaluate performance

--- kotlin/mapFilterReduce.kt
+++ kotlin/mapFilterReduce.kt
@@ -1,8 +1,11 @@
 // Map, Filter, Reduce
+// To increse VM heap size, set environment variable JAVA_OPTS 
+// (For example: 'export JAVA_OPTS="$JAVA_OPTS -Xmx1024M"')
 fun main(args:Array<String>){
+    var start = System.currentTimeMillis()
     var result = 
         // 1. Generating sequence
-        Array(1000000, {i->i})
+        Array(10000000, {i->i})
 
         // 2. Mapping the sequence into another
         .map { n -> n * 2}
@@ -13,6 +16,8 @@ fun main(args:Array<String>){
         // 4. Reducing the sequence
         .fold(0L) { x, y -> x.toLong() + y.toLong()}
 
+    var end = System.currentTimeMillis()
     // As a result
     println(result.toString())
+    println((end-start).toString() + " [msec]")
 }
